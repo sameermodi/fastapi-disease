1c6b2430b1d1d8486923d64ae6debbfaaf21ae25
sameermodi <146810956+sameermodi@users.noreply.github.com>
2025-08-20 19:27:12 +0530

    Updated the server.py

--- fastapi-server/server.py
+++ fastapi-server/server.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
 from model_helper import predict
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["https://your-frontend-domain.com"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict")
 async def get_prediction(file: UploadFile = File(...)):
@@ -15,5 +24,6 @@ async def get_prediction(file: UploadFile = File(...)):
         return {"prediction": prediction}
     except Exception as e:
         return {"error": str(e)}
+
 
 
